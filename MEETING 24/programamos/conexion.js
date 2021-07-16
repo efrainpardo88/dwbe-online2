2b32bb13b8f9225487a489b06802f75ea9c36250
efrainpardoqmcorp <efrain.pardo@quantummaterialscorp.com>
2021-07-15 19:53:31 -0500

    MEETING 24

--- MEETING 24/programamos/conexion.js
+++ MEETING 24/programamos/conexion.js
@@ -1,13 +1,6 @@
 const Sequelize = require('sequelize');
-const path = 'mysql://root@localhost:3306/event_eventos';
-const sequelize = new Sequelize(path, { operatorsAliases: false });
+const path = 'mysql://root@localhost:3306/discografica';
+const sequelize = new Sequelize(path);
 
-sequelize.authenticate().then(() => {
-    console.log('Conectado.');
-}).catch(err => {
-    console.error('Error de conexion:', err);
-}).finally(() => {
-    sequelize.close();
-});
 
 module.exports = sequelize;
--- MEETING 24/programamos/index.js
+++ MEETING 24/programamos/index.js
@@ -3,17 +3,34 @@ var app = express();
 const sequelize = require('./conexion.js');
 
 
-async function findAllRows() {
+async function findAllSongs() {
 
-    sequelize.query("SELECT * FROM inscriptos", { type: sequelize.QueryTypes.SELECT })
+    sequelize.query("SELECT * FROM cancion", { type: sequelize.QueryTypes.SELECT })
         .then(function (productos) {
             console.log(productos);
         })
 }
 
-findAllRows();
+async function findSongByArtist(nombre) {
+
+    return await sequelize.query(`select * 
+    from cancion 
+    where banda_id in(
+        select id
+        from bandamusical
+        where nombre like '%${nombre}%');`, 
+    {
+        type: sequelize.QueryTypes.SELECT 
+    });
+}
+
+app.get('/canciones', async (req, resp) =>{
+
+    const nombre = req.query.nombre;
+    resp.json( await findSongByArtist( nombre ));
+} );
 
 
-app.listen(3000, function () {
-    console.log('Sistema armado en el puerto 3000!');
+app.listen(5050, function () {
+    console.log('Sistema armado en el puerto 5050!');
 });
